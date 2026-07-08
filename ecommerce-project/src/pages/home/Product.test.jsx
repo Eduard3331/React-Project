1191862b5f578f3dc1ae662a3c6e75e4a41f8ccf
Eduard <edybocancea@gmail.com>
2026-07-08 13:35:33 +0300

    Create integration test for HomePage

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,5 +1,5 @@
 import { it, expect, describe, vi, beforeEach } from 'vitest'
-import { logDOM, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Product } from './Product'
 import axios from 'axios';
 import userEvent from '@testing-library/user-event';
